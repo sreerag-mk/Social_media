f7d9c018598ae4405955e54360f3ea515ac39782
sreerag <sreeragmanoj07.com>
2023-10-13 17:39:21 +0530

    created a sign up api for the application

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-/* eslint-disable no-console */
-// eslint-disable-next-line prettier/prettier
-const express = require("express");
-const server = express();
-server.get("/", (req, res) => {
-  res.send("welcome");
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+const users = [];
+app.post('/signup', (req, res) => {
+  const { username, email, password } = req.body;
+  const newUser = {
+    username,
+    email,
+    password,
+  };
+  console.log(newUser)
+  users.push(newUser);
+  return res.status(201).json({ message: 'User registered successfully' });
 });
-server.listen(8000, () => {
-  console.log("Sreerag Manoj");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
+
